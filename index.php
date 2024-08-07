f93fa7c3f5313bbb359e37e17b70bc5cfc92c453
Juraj Smiesko <juraj.smiesko@cern.ch>
2024-08-07 19:52:17 +0200

    Small adjustments

--- index.php
+++ index.php
@@ -78,8 +78,14 @@ foreach ($response['results'] as $result) {
                      fill="currentColor"
                      class="bi bi-geo-alt"
                      viewBox="0 0 16 16">
-                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
-                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32
+                           0 0 1 8 14.58a32 32 0 0
+                           1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304
+                           7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305
+                           1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0
+                           4.314 6 10 6 10"/>
+                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3
+                           3 0 0 0 0 6"/>
                 </svg>
                 <?= $meeting['location']; ?>
               </small>
@@ -89,7 +95,7 @@ foreach ($response['results'] as $result) {
                 <?php if (!empty($meeting['description'])): ?>
                 <a class="link-secondary link-underline link-underline-opacity-0"
                    data-bs-toggle="collapse"
-                   href="#event-desc-<?= $meeting['id']; ?>">Show description</a>
+                   href="#event-desc-<?= $meeting['id']; ?>">Show&nbsp;description</a>
                 <?php endif ?>
               </small>
             </div>
@@ -106,7 +112,8 @@ foreach ($response['results'] as $result) {
       <p class="mt-3">
         The Indico category for all FCC Software and Computing meetings can be found
         <a href="https://indico.cern.ch/category/5666/"
-           class="link-underline link-underline-opacity-0 link-underline-opacity-50-hover">here</a>.
+           class="link-underline link-underline-opacity-0 link-underline-opacity-50-hover"
+           target="_blank">here</a>.
       <p>
     </div>
 
